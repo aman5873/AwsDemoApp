cd357c20673465dd754a9d5121624169f6595ef7
aman5873 <aman@equateme.com>
2023-10-10 11:43:36 +0530

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is changed code.......
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center", color: "#000e5f`" }}>
+        Demo website for aws pipeline
+      </h1>
+      <p style={{ width: "90%", margin: "0 auto", color: "#460170" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        voluptates itaque dolore amet eos. Pariatur quisquam tenetur
+        reprehenderit minus aspernatur, similique architecto eos cumque, rerum
+        consequuntur ducimus quae dignissimos. Porro quisquam architecto
+        veritatis deserunt sit sapiente, sint dolores dignissimos perspiciatis,
+        quibusdam temporibus dolore aperiam expedita eveniet iste rerum esse
+        voluptatum nostrum repudiandae. Facere eaque maxime consequuntur
+        eligendi aspernatur architecto, dignissimos dolorum assumenda error
+        incidunt. Maiores fugiat facilis, hic necessitatibus beatae provident
+        nulla adipisci reprehenderit. Iure non aliquid temporibus perspiciatis
+        omnis molestias velit, veniam nisi reiciendis cum maxime, autem nulla?
+        Laboriosam alias harum eaque placeat ut consequatur unde labore
+        blanditiis nobis!
+      </p>
+    </>
   );
 }
 
